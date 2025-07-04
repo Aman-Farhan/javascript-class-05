d2395c4f0597ac9016133b70e24f308be9b19225
Aman <amanfarhan2008@gmail.com>
2025-07-04 16:57:14 +0500

    second commit

---
+++ app.js
@@ -0,0 +1,152 @@
+// function cook (morning){
+//     return morning ;
+// }
+// function morning (cook){
+//     console.log(cook)
+// }
+// cook("morning me aman hoga")
+// cook("and evening me noman hoga")
+
+// let i = 1
+// for( i = 1 ; i <= 5; i++){
+//     console.log("aman")
+// } 
+
+
+// function greek (username){
+//     return "welcome to javascript "+ username + " !"
+// }
+// function username (name){
+//     let message = greek(name)
+//     console.log(message)
+// }
+// username("Aman Farhan Sir Talal ke Student")
+
+
+// let num = 5;
+// let num1 = 5;
+// let sum = num + num1;
+// console.log("The sum of " + num + " and " + num1 + " is: " + sum);
+
+
+// let num = 5; 
+// let num1 = 10; 
+// let addition = num + num1;
+// let subtraction = num - num1;
+// let multiplication = num * num1;
+// let division = num / num1;
+// let modulus = num % num1;
+
+// console.log("addition " + addition )
+// console.log("subtraction " + subtraction )
+// console.log("multiplication " + multiplication )
+// console.log("division " + division )
+// console.log("modulus " + modulus )
+
+
+
+
+
+// let veriable ;
+// document.write("Value after variable declaration is: " + veriable + "<br>");
+
+// veriable = 5;
+// document.write("Initial value: " + veriable + "<br>");
+
+// veriable++;
+// document.write("Value after increment is: " + veriable + "<br>");
+
+// veriable = veriable + 7;
+// document.write("Value after addition is: " + veriable + "<br>");
+
+// veriable--;
+// document.write("after tha decrement is: " + veriable + "<br>");
+
+// let modulus = veriable % 3;
+// document.write("aThe remainder is: " + modulus + "<br>");
+
+
+
+// let ticket = 600 ;
+// let total = ticket * 5;
+// document.write("Total cost to buy 5 tickets to a movie is " + total + " PKR" + "<br>")
+
+
+
+// let num = 5;
+// let A = 1
+// document.write(num + " X " + A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+// document.write(num + " X " + ++A + " = " + num * A + "<br>");
+
+// console.log(num + " X " + 1 + " = " + num * 1);
+// console.log(num + " X " + 1 + " = " + num * 1);
+
+
+// let number = 5; // Aap yahan koi bhi number rakh sakte hain
+
+//  for (let i = 1; i <= 10; i++) {
+//             document.write(number + " x " + i + " = " + (number * i) + "<br>");
+// }
+
+
+
+// document.write(num + "X" + 1++ + "=" + num)
+// document.write(num + "X" + 1++ + "=" +num++)
+
+
+
+
+
+// let variable ;
+// document.write("Value after variable declaration is: " + variable +"<br>");
+// variable = 5;
+// document.write("Initial value: " + variable +"<br>");
+// variable++;
+// document.write("Value after increment is: " + variable + "<br>");
+// variable = variable + 7 ;
+// document.write("Value after addition is: " + variable + "<br>");
+
+
+
+// let veriable ;
+// console.log("Variable declare hone ke baad value hai:"+ veriable);
+
+// veriable = 5;
+// console.log("Initial value:" + veriable);
+
+// veriable++;
+// console.log("Value after increment is:" + veriable);
+
+// veriable = veriable + 7 ;
+// console.log("Value after addition is:" + veriable)
+
+
+
+// let num1 = prompt();
+// let B = 1; 
+// document.write(num1 + "x" + B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+// document.write(num1 + "x" + ++B + "=" + num1*B + "<br>");
+
+
+
+// let num = prompt("enter a num !");
+// for(let i = 1; i <= 10 ; i++){
+//     document.write(num + "x" + i + "=" + (num*i) + "<br>");
+// }
